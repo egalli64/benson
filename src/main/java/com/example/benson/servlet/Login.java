12f5fc0803770ffa03e6981005914189e4e86428
Emanuele <egalli64@gmail.com>
2022-03-26 22:09:51 +0100

    login

---
+++ src/main/java/com/example/benson/servlet/Login.java
@@ -0,0 +1,30 @@
+package com.example.benson.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.example.benson.bean.User;
+
+@SuppressWarnings("serial")
+@WebServlet("/login")
+public class Login extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String name = request.getParameter("name");
+        String password = request.getParameter("password");
+
+        User user = new User(name == null || name.isBlank() ? "guest" : name, password);
+        request.getSession().setAttribute("user", user);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doPost(request, response);
+    }
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,11 +1,19 @@
 <%@ page language="java" contentType="text/html; charset=utf-8" pageEncoding="utf-8"%>
-<%@ page import="java.time.LocalTime"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <!DOCTYPE html>
 <html>
 <head>
-<title>The Benson Project</title>
+<title>Benson</title>
 </head>
 <body>
     <h1>Welcome to the Benson Project!</h1>
+    <c:choose>
+        <c:when test="${user eq null}">
+            <p>Please, login to proceed</p>
+        </c:when>
+        <c:otherwise>
+            <p>Welcome, ${user.name}!</p>
+        </c:otherwise>
+    </c:choose>
 </body>
 </html>
