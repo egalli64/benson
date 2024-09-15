b797fa85323a7f60a9e9614cce23ae2b5b575679
Emanuele <egalli64@gmail.com>
2024-09-15 19:59:10 +0200

    log

--- src/main/java/com/example/benson/servlet/Hello.java
+++ src/main/java/com/example/benson/servlet/Hello.java
@@ -2,6 +2,9 @@ package com.example.benson.servlet;
 
 import java.io.IOException;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -11,8 +14,13 @@ import jakarta.servlet.http.HttpServletResponse;
 @SuppressWarnings("serial")
 @WebServlet("/hello")
 public class Hello extends HttpServlet {
+    private static final Logger log = LogManager.getLogger(Login.class);
+
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        log.traceEntry("Env URL: " + System.getenv("DB_URL"));
+
         String user = request.getParameter("user");
         response.getWriter().append("Hello, ").append(user == null || user.isBlank() ? "stranger" : user).append('!');
     }
