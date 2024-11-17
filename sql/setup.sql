a9e7f1b467e66f4235ba0c6d6a9c60edad80a3ee
Emanuele <egalli64@gmail.com>
2024-11-17 12:24:30 +0100

    From PostgreSQL to H2

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,17 +1,24 @@
---
--- to access the remote instance from local
--- psql "postgres://<USER>:<PASSWORD>@dpg-crin63bv2p9s738l0ae0-a.frankfurt-postgres.render.com/benson_ldjp"
---
+-- H2 Database setup
+-- java -cp h2.jar org.h2.tools.RunScript -url jdbc:h2:./benson -script setup.sql
 
 drop schema if exists benson cascade;
-create schema benson;
+drop user if exists benson;
 
-create table benson.user (
-	user_id serial primary key,
+create user benson password 'password';
+create schema benson;
+grant all on schema benson to benson;
+
+use benson;
+
+drop table if exists account;
+
+create table account (
+	account_id identity primary key,
 	name text unique not null,
 	password text not null,
 	administrator boolean not null
 );
 
-insert into benson.user (name, password, administrator) values('root', 'il46qnBMTuYj+q8NE9OI5Q==', true);
-insert into benson.user (name, password, administrator) values('guest', 'RhrUE1U/mdh4TYGk/E35fA==', false);
+insert into account (name, password, administrator) values
+	('root', 'il46qnBMTuYj+q8NE9OI5Q==', true),
+	('guest', 'RhrUE1U/mdh4TYGk/E35fA==', false);
