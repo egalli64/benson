920a00ef105279b6d20038ed842b602e6eada5eb
Emanuele <egalli64@gmail.com>
2024-12-14 22:33:40 +0100

    simplify H2 url

--- start.sh
+++ start.sh
@@ -8,7 +8,7 @@ mkdir -p /app/h2
 if [ ! -f "$DB_FILE" ]; then
     echo "Initializing database..."
     java -cp /usr/local/tomcat/lib/h2.jar org.h2.tools.RunScript \
-        -url "jdbc:h2:file:/app/h2/benson;DB_CLOSE_ON_EXIT=FALSE;AUTO_SERVER=TRUE" \
+        -url "jdbc:h2:file:/app/h2/benson;AUTO_SERVER=TRUE" \
         -user sa \
         -script "$SQL_SCRIPT"
 else
